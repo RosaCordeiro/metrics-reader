2ac2a29553fb836de7cbcf40822af49d1ade1168
Guilherme Cordeiro <guilherme.cordeiro@clamed.com.br>
2025-10-29 13:35:20 -0300

    Ajuste de estrutura de pastas e nomenclatura de arquivos

--- src/presentation/routes/metrics.routes.ts
+++ src/presentation/routes/metrics.routes.ts
@@ -1,7 +1,5 @@
-import { metricsHandler } from './../../shared/providers/metrics/instrumentation';
-import { ItsAliveController } from "@/presentation/controllers/its-alive.controller";
 import { Router } from "express";
-import { MetricsController } from '../controllers/MetricsController';
+import { MetricsController } from '../controllers/metrics.controller';
 
 const metricsRouter = Router();
 
